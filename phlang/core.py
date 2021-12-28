d3f31d16050c38dfa14518642e865ff0604fabc7
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2021-12-28 08:46:59 -0600

    Epic cool gamer commit

--- phlang/core.py
+++ phlang/core.py
@@ -1,5 +1,4 @@
 
-from phlang.strings import *
 import os
 import string
 import os
@@ -11,6 +10,34 @@ LETTERS = string.ascii_letters
 LETTERS_DIGITS = LETTERS + DIGITS
 
 
+
+
+def string_with_arrows(text, pos_start, pos_end):
+	result = ''
+
+	# Calculate indices
+	idx_start = max(text.rfind('\n', 0, pos_start.idx), 0)
+	idx_end = text.find('\n', idx_start + 1)
+	if idx_end < 0: idx_end = len(text)
+	
+	# Generate each line
+	line_count = pos_end.ln - pos_start.ln + 1
+	for i in range(line_count):
+		# Calculate line columns
+		line = text[idx_start:idx_end]
+		col_start = pos_start.col if i == 0 else 0
+		col_end = pos_end.col if i == line_count - 1 else len(line) - 1
+
+		# Append to result
+		result += line + '\n'
+		result += ' ' * col_start + '^' * (col_end - col_start)
+
+		# Re-calculate indices
+		idx_start = idx_end
+		idx_end = text.find('\n', idx_start + 1)
+		if idx_end < 0: idx_end = len(text)
+
+	return result.replace('\t', '')
 
 class Error:
   def __init__(self, pos_start, pos_end, error_name, details):
@@ -58,7 +85,7 @@ class RTError(Error):
       pos = ctx.parent_entry_pos
       ctx = ctx.parent
 
-    return 'Traceback (most recent call last):\n' + result
+    return 'Traceback (most recent call):\n' + result
 
 class Position:
   def __init__(self, idx, ln, col, fn, ftxt):
@@ -2197,7 +2224,7 @@ global_symbol_table.set("cls", BuiltInFunction.clear)
 global_symbol_table.set("is_num", BuiltInFunction.is_number)
 global_symbol_table.set("is_str", BuiltInFunction.is_string)
 global_symbol_table.set("is_list", BuiltInFunction.is_list)
-global_symbol_table.set("is_fun", BuiltInFunction.is_function)
+global_symbol_table.set("is_func", BuiltInFunction.is_function)
 global_symbol_table.set("append", BuiltInFunction.append)
 global_symbol_table.set("pop", BuiltInFunction.pop)
 global_symbol_table.set("extend", BuiltInFunction.extend)
--- shell.py
+++ shell.py
@@ -1,23 +1,29 @@
 import phlang.core as core, sys
 
-try:
-	if sys.argv[1]:
-		with open(sys.argv[1], "r") as f:
-			text = f.read() 
-		core.run(sys.argv[1], text)
-except:
-	try:
-		while True:
-			text = input('phlang > ')
-			if text.strip() == "": continue
-			result, error = core.run('<stdin>', text)
 
-			if error:
-				print(error.as_string())
-			elif result:
-				if len(result.elements) == 1:
-					print(repr(result.elements[0]))
-				else:
-					print(repr(result))
-	except Exception as e:
-		print("Uncaught exception:", e)
+def main():
+	try:
+		if sys.argv[1]:
+			with open(sys.argv[1], "r") as f:
+				text = f.read() 
+			core.run(sys.argv[1], text)
+	except:
+		try:
+			while True:
+				text = input('phlang > ')
+				if text.strip() == "": continue
+				result, error = core.run('<stdin>', text)
+
+				if error:
+					print(error.as_string())
+				elif result:
+					if len(result.elements) == 1:
+						print(repr(result.elements[0]))
+					else:
+						print(repr(result))
+		except Exception as e:
+			print("Uncaught exception:", e)
+			main()
+
+if __name__ == "__main__":
+	main()
